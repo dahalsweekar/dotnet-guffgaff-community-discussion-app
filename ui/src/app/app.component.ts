a1fbbbe818b9551281d4815c9af1c1c454a3ad8f
Sweekar Dahal <99968233+dahalsweekar@users.noreply.github.com>
2025-09-09 10:08:03 +0545

    page layout fixes

---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './main/header.component/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  protected title = 'ui';
+}
