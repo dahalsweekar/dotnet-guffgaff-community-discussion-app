69564c544e442510b322560394e754282ec488e5
Sweekar Dahal <99968233+dahalsweekar@users.noreply.github.com>
2025-09-13 17:00:16 +0545

    api issue fix

--- backend/GuffGaff.WebAPI/GuffGaff.Services/Program.cs
+++ backend/GuffGaff.WebAPI/GuffGaff.Services/Program.cs
@@ -1,47 +1,6 @@
-using GuffGaff.Database.DBContext;
-using GuffGaff.Services.Interfaces;
-using GuffGaff.Services.Services;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<GuffGaffDBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontEnd", policy =>
-    {
-        policy.WithOrigins("http://localhost:4200")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.PropertyNamingPolicy = null;
-    });
-
-builder.Services.AddAuthorization();
-builder.Services.AddScoped<IPostServices, PostServices>();
-builder.Services.AddScoped<IUserServices, UserServices>();
-builder.Services.AddScoped<ICommentServices, CommentServices>();
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/", () => "Hello World!");
 
-app.UseHttpsRedirection();
-app.UseCors("AllowFrontEnd");
-app.UseAuthorization();
-app.UseAuthentication();
-app.MapControllers();
 app.Run();
